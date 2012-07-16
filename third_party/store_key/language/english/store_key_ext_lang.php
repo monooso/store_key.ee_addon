40aa7dfe7e9d88ecd467b3c904c2607eecc366a5
Stephen Lewis <stephen@experienceinternet.co.uk>
2012-07-16 21:00:46 +0100

    Create error and exception language strings.

--- third_party/store_key/language/english/store_key_ext_lang.php
+++ third_party/store_key/language/english/store_key_ext_lang.php
@@ -12,6 +12,15 @@ $lang = array(
   'store_key_extension_name' => 'Store Key',
   'store_key_extension_description' => 'Generates license keys for items purchased through the Exp:resso Store.',
 
+  // Error messages.
+  'error__missing_order_item_details' => 'Missing order item details.',
+  'error__no_order_items' => 'Order does not contain any items.',
+  'error__unable_to_generate_license_key' => 'Unable to generate license key.',
+
+  // Exception messages.
+  'exception__generate_license_key_invalid_parameters' => 'Invalid parameters passed to "generate_license_key".',
+  'exception__save_license_key_invalid_parameters' => 'Invalid parameters passed to "save_license_key".',
+
   // All done.
   '' => ''
 );
